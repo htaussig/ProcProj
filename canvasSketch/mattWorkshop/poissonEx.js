88bbb0b89a51e7edad44cc7ac37b83b68183093c
harrytaussig <htaussig@haverford.edu>
2019-10-22 00:01:02 -0400

    flowers

--- canvasSketch/mattWorkshop/poissonEx.js
+++ canvasSketch/mattWorkshop/poissonEx.js
@@ -29,7 +29,7 @@ const sketch = () => {
 
 //random.setSeed(123);
 //var points = createGrid();
-var p = new poisson([100, 100, 100], 15, 25, 30); //seems to work poorly with decimals
+var p = new poisson([100, 100, 100], 11, 23, 30); //seems to work poorly with decimals
 var points = p.fill();
 console.log(points);
 // const tokenElement = points[points.length - 1];
@@ -38,12 +38,14 @@ console.log(points);
 //points = random.shuffle(points);
 const margin = 250; 
 
+const textArray = ['🌺','🌻','🌼','🏵','💠','🌿️'];
+const colArray = ['#cda1fd', '#fff0f0', '#fbcefd', '#efaef4'];
   //returns a render function (pure function)
   //return { extension: '.svg', data: -------};
   return ({ context, width, height }) => {
 
     //background is transparent, not white
-    context.fillStyle = 'white';
+    context.fillStyle = 'black';
     context.fillRect(0, 0, width, height);
 
     points.sort((a, b) => a[2] - b[2]);
@@ -71,9 +73,31 @@ const margin = 250;
 
       const l = (w * 60) + 20;
 
-      context.fillStyle = `hsl(210, ${l}%, ${l}%)`;  //`rgb(${w * 255}, 1, 1)`;
-      context.fillRect(x, y, 100, 100);
+      const sizeMult = .1 / ((w / 2) + 0.5);
+
+      const text = random.pick(textArray);
+
+      //context.fillStyle = `hsl(210, ${l}%, ${l}%)`;  //`rgb(${w * 255}, 1, 1)`;
+      //context.fillRect(x, y, 100 * sizeMult, 100 * sizeMult);
+      context.save();
+      context.translate(x - (width * 2), y - (height * 2) + 80); //draw the original shape off the screen
+      const rotation = random.range(-Math.PI / 4, Math.PI / 4);
+      context.rotate(rotation);
     
+      const color = random.pick(colArray);
+      //`hsl(210, ${l}%, ${l}%)`
+      context.shadowColor = color;
+      
+      context.shadowBlur = 0;
+      context.shadowOffsetX =  width * 2;
+      context.shadowOffsetY =  height * 2;
+
+      context.font = `${(sizeMult / 2) * width}px "Arial"`;
+      //context.scale(sizeMult, sizeMult)
+      context.textAlign = 'center';
+      context.fillText(text, 0, 0);
+
+      context.restore();
     
     });
     //console.log(points);
